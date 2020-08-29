d25df09260dd1d7d75af3cecbb1e231e34af5437
winsoso <52603200+winsoso@users.noreply.github.com>
2020-08-30 06:31:29 +0800

    Update and rename gulimall-pms/src/main/java/com/atguigu/gulimall/pms/controller/PmsOssController.java to Gulimall-pms/src/主要/爪哇/com/阿吉古/古里马尔/每秒/控制者/PmsOssController.java
    删除阿里云AccessKeySecret

--- Gulimall-pms/src/主要/爪哇/com/阿吉古/古里马尔/每秒/控制者/PmsOssController.java
+++ Gulimall-pms/src/主要/爪哇/com/阿吉古/古里马尔/每秒/控制者/PmsOssController.java
@@ -26,7 +26,7 @@ import java.util.Map;
 public class PmsOssController {
 
     String accessId = "LTAIWlEcp18EWE74";      // 请填写您的AccessKeyId。
-    String accessKey = "5s1zIEpjLLZp4wXvQx327dCXiD319l"; // 请填写您的AccessKeySecret。
+    String accessKey = "xxxxx"; // 请填写您的AccessKeySecret。
     String endpoint = "oss-cn-beijing.aliyuncs.com"; // 请填写您的 endpoint。
     String bucket = "gulimall-winsoso";                    // 请填写您的 bucketname 。
     String host = "https://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
